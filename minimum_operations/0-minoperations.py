dfaa1a1882d0491500da57f7dccbaaa79ec07667
Isaiah-Essien <i.essien@alustudent.com>
2023-09-11 11:09:18 +0200

    .\0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,28 +4,30 @@ A script to calculate the fewest number of operations needed
 to result in exactly n H characters in the file.
 """
 
-def minOperations(n):
-    if n <= 1:
-        return 0
-    
-    current = 1  # Current number of H characters
-    clipboard = current  # Number of characters in the clipboard
-    operations_count = 0  # Total operations count
-    
-    while current < n:
-        # If n is divisible by the current number of characters,
-        # we can double the characters using copy all and paste (2 ops).
-        if n % current == 0:
-            clipboard = current
-            current *= 2
-            operations_count += 2
-        else:
-            # If not divisible, we paste the characters in the clipboard (1 op).
-            current += clipboard
-            operations_count += 1
-    
-    return operations_count
+def calculateMinOperations(target):
+    """
+    Calculates the minimum number of operations needed
+    to achieve the target number of characters in the file.
 
-# Example usage:
-n = 9
-print(minOperations(n))  # Output: 6
+    :param target: The desired number of characters.
+    :type target: int
+    :return: The minimum number of operations required.
+    :rtype: int
+    """
+    if target <= 1:
+        return 0
+
+    current = 1
+    clipboard = current
+    mN = 0
+
+    while current < target:
+        if target % current == 0:
+            clipboard = current
+            current = 2 * clipboard
+            mN += 2
+        else:
+            current += clipboard
+            mN += 1
+
+    return mN
